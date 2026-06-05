871f80707c6c5e01155fdd9e58b30c88ea8da7d2
jae <96039674+jae-park-au@users.noreply.github.com>
2026-06-05 10:38:32 +1000

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        return_list = []
+        MAX_DEPTH = len(nums)
+
+        def backtrack(cur_state: list, depth: int):
+            if depth == MAX_DEPTH:
+                return_list.append(cur_state[:])
+                return 
+
+            backtrack(cur_state[:], depth + 1)
+
+            next_state = cur_state[:]
+            next_state.append(nums[depth])
+
+            backtrack(next_state, depth + 1)
+            
+
+        backtrack(list(), 0)
+
+        return return_list
